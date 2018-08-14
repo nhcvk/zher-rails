c9c7ac0e1ca87b014a3e8f4a5cd4f9938df3c56b
Sven den Boer <denboer.sven@gmail.com>
2018-08-14 13:24:36 +0800

    database ready plz check

---
+++ db/migrate/20180814035834_create_places.rb
@@ -0,0 +1,17 @@
+class CreatePlaces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.text :description
+      t.string :main_photo_url
+      t.string :photo_urls, array: true, default: []
+      t.references :local, foreign_key: true
+      t.references :city, foreign_key: true
+      t.references :bookmark, foreign_key: true
+      t.float :latitude
+      t.float :longitude   
+
+      t.timestamps
+    end
+  end
+end
