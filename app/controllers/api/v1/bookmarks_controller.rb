b3003e1bf9f8f94cdd65d8d15f80f8bac0fb33a3
Sven den Boer <denboer.sven@gmail.com>
2018-08-20 19:00:54 +0800

    Added a JSON to bookmark

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::BookmarksController < Api::V1::BaseController
         # @bookmark.user = @user
         # @bookmark.place = @place
         @bookmark.save
-        render json: @bookmark, :include => {:city => {:only => :name}}
+        render json: @bookmark, :include => {:city => {:only => :name}, :place => {:only => :city_id}}
     end
 
     def destroy
