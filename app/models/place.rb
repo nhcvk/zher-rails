e45f64599e838188906dba188538be81fd90bfbd
Sven den Boer <denboer.sven@gmail.com>
2018-08-21 21:33:50 +0800

    added validations to photo upload

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,9 @@ class Place < ApplicationRecord
     belongs_to :city
 
     validates :main_photo_url, presence: :true
+    validates :name, presence: :true
+    validates :description, presence: :true
+    validates :longitude, presence: :true
+    validates :latitidue, presence: :true
     # validates :photo_urls, presence: :true, length: { minimum: 1 }
 end
